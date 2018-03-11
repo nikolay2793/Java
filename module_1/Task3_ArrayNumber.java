3c569b098086f6613cff115437d70bfa7c62bef0
Nikolay731 <nikolaysevrukov@gmail.com>
2018-03-11 02:10:52 +0200

    upd002

--- module_1/Task3_ArrayNumber.java
+++ module_1/Task3_ArrayNumber.java
@@ -3,21 +3,16 @@ import java.util.Arrays;
 
 public class Task3_ArrayNumber {
 
-   public static int[] array = new int[16];
+    public static int[] array = new int[16];
 
-   public static int[] secondArray = new int[array.length];
+    public static int[] secondArray = new int[array.length];
 
-   public static int number = 0;
+    public static int number = 0;
 
-   public static int digit;
-
-
+    public static int digit;
 
 
-   private static boolean ok = false;
-
-//    public static int next;
-//    public static int prev;
+    private static boolean ok = false;
 
 
     public static void main(String[] args) {
@@ -26,18 +21,18 @@ public class Task3_ArrayNumber {
 
         for (int i = 0; i < array.length; i++) {
 
-            int z = rand.nextInt(9)+1;
+            int z = rand.nextInt(9) + 1;
             array[i] = z;
             System.out.print(array[i] + ", ");
         }
 
+        compare();
 
         digit = array[0];
 
-
-        for(int i = 0; i < array.length; i++) {
-
+        for (int i = 0; i < array.length; i++) {
             check();
+            compare();
         }
 
         System.out.println("\n");
@@ -46,31 +41,31 @@ public class Task3_ArrayNumber {
 
     public static void check() {
 
-
         for (int j = 0; j < array.length; j++) {
 
             digit = array[j];
             secondArray[j] = digit;
 
-            System.out.print("digit" + digit + ", ");
-            System.out.print("secondArray" + secondArray[j] + ", ");
+//            System.out.print("digit" + digit + ", ");
+//            System.out.print("secondArray" + secondArray[j] + ", ");
 
-            if(ok)
-            {
-                number ++;
+            if (ok) {
+                number++;
             }
         }
     }
 
     public static void compare() {
 
-        for(int i = 0; i < array.length; i++)
-        {
-            if(digit != secondArray[i])
-            {
-               ok = true;
-                System.out.print(secondArray[i]);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i+1; j < array.length; j++) {
+                if (array[i] != secondArray[j])
+                {
+                    ok = true;
+
+                }
             }
+            System.out.println("secondArray" + secondArray[i]);
         }
     }
 }
